04a5a70b63e04b3bc9360efb4c177cd8d67ac861
Villawolf <juanLobito@gmail.com>
2025-10-21 10:24:30 -0500

    agregar vista material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CustomerComponent } from './pages/customer-component/customer-componente';
 import { SupplierComponent } from './pages/supplier-component/supplier-componente';
 import { ProjectComponent } from './pages/project-component/project-component';
+import { MaterialComponente } from './pages/material-component/material-componente';
 import { ProjectEditComponent } from './pages/project-component/project-edit-componente/project-edit-componente';
 import { CustomerEditComponent } from './pages/customer-component/customer-edit-componente/customer-edit-componente';
 import { SupplierEditComponent } from './pages/supplier-component/supplier-edit-componente/supplier-edit-componente';
+import { MaterialEditComponent } from './pages/material-component/material-edit-componente/material-edit-componente';
+
+
 
 export const routes: Routes = [
     {
@@ -25,6 +29,13 @@ export const routes: Routes = [
         children: [
             { path: 'new', component: ProjectEditComponent },
             { path: 'edit/:id', component: ProjectEditComponent }
+        ]
+    },
+    {
+        path: 'pages/material', component: MaterialComponente,
+        children: [
+            { path: 'new', component: MaterialEditComponent },
+            { path: 'edit/:id', component: MaterialEditComponent }
         ]
     }
 ];
